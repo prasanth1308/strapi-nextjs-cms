0f41a63d46920d4e41f3de64d86c39fad4569b77
Prasanth Ravi <prasanth_ravi@trimble.com>
2020-09-22 08:23:17 +0530

    Added code from tutorial

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '77eef581c3cb897b5c263636c3526962'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e8099888-07e7-42ed-ac4b-d775ebd61dd0'
+};
